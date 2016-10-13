c4c52951a6b60a417ce4f361384c10d6cae90aaa
sethvincent <sethvincent@gmail.com>
2016-10-13 15:25:57 -0700

    try hash routing

--- README.md
+++ README.md
@@ -2,3 +2,16 @@
 
 Trying out (soon to be released) v4 of [choo](https://github.com/yoshuawuyts/choo)
 
+Currently pointing at a fork for a fix to hash routing: 
+
+## anchor links
+
+```
+budo anchor-links.js
+```
+
+## hash routing
+
+```
+budo hash-routes.js
+```
---
+++ hash-routes.js
@@ -0,0 +1,53 @@
+var choo = require('choo')
+var html = require('choo/html')
+
+var app = choo({ hash: true })
+app.use(require('choo-log')())
+
+app.model({
+  namespace: 'app',
+  state: {},
+  reducers: {}
+})
+
+app.router({ default: '#' }, [
+  ['#', wrapper(home)],
+  ['#hey', wrapper(hey)],
+  ['#ok', wrapper(ok)]
+])
+
+function wrapper (page) {
+  return function (state, prev, send) {
+    console.log('page?', page.name)
+    return page(state, prev, send)
+  }
+}
+
+function home (state, prev, send) {
+  return html`<div>
+    <h1>Home</h1>
+    <a href="/#hey">hey</a>
+    <a href="/#ok">ok</a>
+  </div>`
+}
+
+function hey (state, prev, send) {
+  return html`<div>
+    <h1>Hey!</h1>
+    <a href="/#hey">hey</a>
+    <a href="/#ok">ok</a>
+    <a href="/">home</a>
+  </div> `
+}
+
+function ok (state, prev, send) {
+  return html`<div>
+    <h1>OK!</h1>
+    <a href="/#hey">hey</a>
+    <a href="/#ok">ok</a>
+    <a href="/">home</a>
+  </div>`
+}
+
+var tree = app.start()
+document.body.appendChild(tree)
